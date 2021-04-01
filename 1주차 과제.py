06a46b7ec1bd0c5ee9cfacebe3039f16c102e18e
SuYoung888 <81761832+SuYoung888@users.noreply.github.com>
2021-04-01 23:41:27 +0900

    Add files via upload

---
+++ 1주차 과제.py
@@ -0,0 +1,9 @@
+station = '사당'
+print(station + "행 열차가 들어오고 있습니다.")
+station = '신도림'
+print(station + "행 열차가 들어오고 있습니다.")
+station = '인천공항'
+print(station + "행 열차가 들어오고 있습니다.")
+from random import *
+print('오프라인 스터디 모임 날짜는 매월 ' + str(int(random() * 25) + 4) + '일로 선정되었습니다.')
+
